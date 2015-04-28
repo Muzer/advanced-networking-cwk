3727cd42000c0c0253ae210fda85573590ee3f77
Murray Colpman <muzer@tim32.org>
2015-04-29 00:49:29 +0100

    Add xbee client

---
+++ src/xbee/Client.py
@@ -0,0 +1,38 @@
+'''
+Created on 14 Apr 2015
+@author: alex
+'''
+from comms import listener, timeOuter, sendMessage, broadcastName, get_address
+import thread
+import xbee
+import serial
+
+username = raw_input("What is your name? ")
+nameLocatorMap = {}
+nameTimeMap = {}
+#start process updating naming map
+serialStr = '/dev/ttyUSB0'
+serialPort = serial.Serial(serialStr, 9600)
+zigbee = xbee.ZigBee(serialPort)
+
+
+myAddr = get_address(zigbee)
+bcAddr = 0x000000000000FFFF
+print(myAddr)
+#nameLocatorMap[username] = myIP
+thread.start_new_thread(listener, (zigbee, nameLocatorMap,nameTimeMap,bcAddr,myAddr))
+thread.start_new_thread(timeOuter, (zigbee, nameTimeMap, username,myAddr,bcAddr))
+#start process timing out name mappings
+
+while True:
+    print
+    recipient = raw_input("Send message to: ")
+    
+    # check recipient name is registered
+    if not nameLocatorMap.has_key(recipient):
+        print("Username '" + recipient + "' is not registered")
+        continue
+    
+    data = raw_input("Message for " + recipient + ": ")
+    # send message
+    sendMessage(zigbee,username,data,nameLocatorMap.get(recipient),myAddr)
---
+++ src/xbee/comms.py
@@ -0,0 +1,115 @@
+'''
+Created on 15 Apr 2015
+
+@author: alex
+'''
+import json
+import time
+import calendar
+import struct
+
+
+#values or tags for use in json message encoding
+TYPE = "type"
+SENDER = "sender"
+BODY = "body"
+MESSAGE = "message"
+NAMING = "naming"
+USERNAME = "username"
+IDENTIFIER = "identifier"
+
+LOOP_TIME = 5
+MAX_LOOPS = 4
+
+def get_frame(zigbee, name):
+    while True:
+        frame = zigbee.wait_read_frame()
+        if frame["id"] != name:
+            continue
+        return frame
+
+def get_address(zigbee):
+    # First, wait for the XBee to join a network
+    print("Waiting for network...")
+    while True:
+        zigbee.at(command='MY')
+        my = get_frame(zigbee, 'at_response')["parameter"]
+        if my != '\xff\xfe':
+            break
+        time.sleep(LOOP_TIME)
+    print("Got network.")
+    zigbee.at(command='SH')
+    sh = struct.unpack(">L", get_frame(zigbee, 'at_response')["parameter"])[0]
+    zigbee.at(command='SL')
+    sl = struct.unpack(">L", get_frame(zigbee, 'at_response')["parameter"])[0]
+    return (sh << 32) + sl
+    
+def broadcastName(zigbee, username,myAddr,bcAddr):
+    "send the username to everyone on the network"
+    data = json.dumps({TYPE:NAMING, USERNAME:username, IDENTIFIER:myAddr}) + '\n'
+    zigbee.send("tx", dest_addr_long=struct.pack(">Q", bcAddr), dest_addr='\xff\xfe', data=data)
+    return
+
+def unicastName(zigbee, username,recipientID,myAddr):
+    "send the username to one person on the network"
+    data = json.dumps({TYPE:NAMING, USERNAME:username, IDENTIFIER:myAddr})
+    zigbee.send("tx", dest_addr_long=struct.pack(">Q", recipientID), dest_addr='\xff\xfe', data=data)
+    return
+
+def sendMessage(zigbee, senderName, message, destinationAddr, myAddr):
+    data = json.dumps({TYPE:MESSAGE, SENDER:senderName, BODY:message})
+    zigbee.send("tx", dest_addr_long=struct.pack(">Q", destinationAddr), dest_addr='\xff\xfe', data=data)
+    return
+
+def timeOuter(zigbee, nameTimeMap,myName,myAddr,bcAddr):
+    while True:
+        broadcastName(zigbee, myName, myAddr, bcAddr)
+        now = calendar.timegm(time.gmtime())
+        toRemove = set()
+        for name in nameTimeMap.iterkeys():
+            nameTime = nameTimeMap.get(name)
+            if now > nameTime + (LOOP_TIME * MAX_LOOPS):
+                toRemove.add(name)
+        for name in toRemove:
+            nameTimeMap.pop(name)
+            print(name + " removed")
+        time.sleep(LOOP_TIME)
+    return
+
+def listener(zigbee, nameLocatorMap, nameTimeMap, bcAddr, myAddr):
+    #build a listener
+    print("listening for names/messages")
+    while True:
+        #wait for a connection and read it
+        frame = get_frame(zigbee, 'rx')
+        #process it
+        jData = json.loads(frame["rf_data"])
+        if jData.has_key(TYPE):
+            msgType = jData.get(TYPE)
+            if msgType == NAMING:
+                #if its a name update, store it
+                if jData.has_key(USERNAME) and jData.has_key(IDENTIFIER):
+                    username = jData.get(USERNAME)
+                    identifier = jData.get(IDENTIFIER)
+                    if username not in nameLocatorMap.keys():
+                        print(username + " has joined the chat")
+                    #store the name
+                    nameLocatorMap[username] = identifier
+                    #store time received
+                    nameTimeMap[username] = calendar.timegm(time.gmtime())
+                else:
+                    print("Unable to read name")
+            elif msgType == MESSAGE:
+                #if its a message, print it
+                if jData.has_key(SENDER) and jData.has_key(BODY):
+                    senderName = jData.get(SENDER)
+                    message = jData.get(BODY)
+                    print(senderName + ": " + message)
+                else:
+                    print("Unable to read message")
+            else:
+                print("unknown message type")
+
+
+    print("Listener failed. Restart program")    
+    return
